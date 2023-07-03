60f6f81071b03fd191ba9ad05c4a2333646f0e3c
MarquezE92 <vsuykyasmbp@hotmail.com>
2023-07-03 02:07:09 -0300

    complete comments in backend

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,19 +1,14 @@
-const { Router } = require('express');
-const router = Router();
-const express = require('express');
-router.use(express.json());
-const { routePostMessage } = require('./postMessage');
+const { Router } = require('express'); //* El módulo Router permite definir rutas en una aplicación Express.
+const router = Router();               //* Creamos una instancia del enrutador
+const express = require('express');    //* Importamos Express
+router.use(express.json());            //* Utilizamos el método json() para parsear las solicitudes entrantes con formato JSON.
+const { routePostMessage } = require('./postMessage');      //* Importamos las funciones para el manejo de las rutas
 const { routeGetAllMessages } = require('./getAllMessages');
 
 
-
-
-router.post("/message", routePostMessage);
+router.post("/message", routePostMessage);      //* Se registran las rutas de GET y POST
 router.get("/messages", routeGetAllMessages);
 
 
 
-
-
-
-module.exports = router;
+module.exports = router;    //* Se exporta el enrutador
